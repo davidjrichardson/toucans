2c046bdb1fc7fe3b9a346a0d2e7439a012e3c143
David Richardson <david@tankski.co.uk>
2018-12-30 16:42:48 +0000

    Start modularising the home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,34 @@
 from django.db import models
-
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class Footer(models.Model):
+    facebook_url = models.URLField(null=True, blank=True)
+    twitter_url = models.URLField(null=True, blank=True)
+
+    panels = [
+        FieldPanel('facebook_url'),
+        FieldPanel('twitter_url'),
+    ]
+
+    def __str__(self):
+        return 'Footer URLs'
+
+
+class BlogPage(Page):
+    pass
 
 
 class HomePage(Page):
-    pass
+    description = models.TextField(max_length=400, default='')
+
+    @property
+    def posts(self):
+        return BlogPage.objects.live().order_by('-first_published_at')[:5]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full"),
+    ]
