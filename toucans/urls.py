2b8db01f1df42090fd4bc8aa6842810a449aac78
David Richardson <david@tankski.co.uk>
2019-03-12 11:27:04 +0000

    Add favicons and some OpenGraph stuff to base template

--- toucans/urls.py
+++ toucans/urls.py
@@ -3,15 +3,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
 urlpatterns = [
-    url(r'^django-admin/', admin.site.urls),
+    url(r'^sitemap\.xml', sitemap),
+    url(r'^admin/', admin.site.urls),
 
-    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
