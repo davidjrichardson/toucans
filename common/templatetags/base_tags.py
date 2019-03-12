87426a4580ccb5443d393153f1f126c579be92ce
David Richardson <david@tankski.co.uk>
2019-03-12 13:10:06 +0000

    Add OpenGraph meta tags to page templates

--- common/templatetags/base_tags.py
+++ common/templatetags/base_tags.py
@@ -1,4 +1,6 @@
+import mimetypes
 from collections import OrderedDict
+
 from django import template
 
 from home.models import Footer, BlogIndexPage, BlogPage, StandingsPage, SchedulePage, StandingsIndexPage, GenericPage, \
@@ -9,6 +11,18 @@ register = template.Library()
 
 def has_menu_children(page):
     return page.get_children().live().in_menu().exists()
+
+
+@register.simple_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+
+@register.filter
+def get_image_mime_type(image):
+    return mimetypes.guess_type(image.url)[0]
 
 
 @register.filter
