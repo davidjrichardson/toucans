f24b6b59e3013f97a0d4a351ecee744797e693b6
David <david@tankski.co.uk>
2022-09-20 08:44:34 +0100

    chore: Bump wagtail to 2.15.5

--- toucans/settings/base.py
+++ toucans/settings/base.py
@@ -173,6 +173,11 @@ BOWER_INSTALLED_APPS = [
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "toucans"
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'wagtail.search.backends.database',
+    }
+}
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
