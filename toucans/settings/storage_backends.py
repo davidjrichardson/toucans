e78c952941635a0f29166051e0aeeea6e543b408
David Richardson <david@tankski.co.uk>
2025-01-12 17:00:43 +0000

    style: Fix file formatting using ruff

--- toucans/settings/storage_backends.py
+++ toucans/settings/storage_backends.py
@@ -6,7 +6,7 @@ class StaticStorage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
     custom_domain = (
-        f'{setting("AWS_S3_CUSTOM_DOMAIN")}/{setting("AWS_STORAGE_BUCKET_NAME")}'
+        f"{setting('AWS_S3_CUSTOM_DOMAIN')}/{setting('AWS_STORAGE_BUCKET_NAME')}"
     )
 
 
@@ -14,6 +14,6 @@ class MediaStorage(S3Boto3Storage):
     location = "media"
     default_acl = "public-read"
     custom_domain = (
-        f'{setting("AWS_S3_CUSTOM_DOMAIN")}/{setting("AWS_STORAGE_BUCKET_NAME")}'
+        f"{setting('AWS_S3_CUSTOM_DOMAIN')}/{setting('AWS_STORAGE_BUCKET_NAME')}"
     )
     file_overwrite = False
